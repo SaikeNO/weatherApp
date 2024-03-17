ad47f8eb9d128079a1d905cd2d24f656578ae0ca
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-03-17 13:39:43 +0100

    fe: configure enviroments

--- weatherapp.client/src/app/services/city.service.ts
+++ weatherapp.client/src/app/services/city.service.ts
@@ -3,33 +3,34 @@ import { Injectable } from "@angular/core";
 import { City } from "../interfaces/City";
 import { catchError, throwError } from "rxjs";
 import { CreateCity } from "../interfaces/CreateCity";
+import { environment } from '../../environments/environment';
 
 @Injectable({providedIn: "root"})
 export class CityService {
   constructor(private http: HttpClient) { }
 
   getCities() {
-    return this.http.get<City[]>("/api/city")
+    return this.http.get<City[]>(`${environment.apiURL}/city`)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 
   createCity(city: CreateCity) {
-    return this.http.post<City>("api/city", city)
+    return this.http.post<City>(`${environment.apiURL}/city`, city)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 
   deleteCity(id: number) {
-    return this.http.delete<void>(`api/city/${id}`)
+    return this.http.delete<void>(`${environment.apiURL}/city/${id}`)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 
   getCity(id: number) {
-    return this.http.get<City>(`/api/city/${id}`)
+    return this.http.get<City>(`${environment.apiURL}/city/${id}`)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 
   updateCity(id:number, city: CreateCity) {
-    return this.http.put<City>(`/api/city/${id}`, city)
+    return this.http.put<City>(`${environment.apiURL}/city/${id}`, city)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 
--- weatherapp.client/src/app/services/weather.service.ts
+++ weatherapp.client/src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { Weather } from '../interfaces/Weather';
+import { environment } from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class WeatherService {
@@ -9,7 +10,7 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeather(city: string) {
-    return this.http.get<Weather>(`/api/weather/${city}`)
+    return this.http.get<Weather>(`${environment.apiURL}/weather/${city}`)
       .pipe(catchError((e: HttpErrorResponse) => throwError(() => e)));
   }
 }
